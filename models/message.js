92f859c22d04c0ecec85c51c106d328744d83bbe
Harsa-2004 <147088869+Harsa-2004@users.noreply.github.com>
2025-03-04 17:53:39 +0530

    Add files via upload

---
+++ models/message.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  projectId: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
+  sender: { type: String, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
