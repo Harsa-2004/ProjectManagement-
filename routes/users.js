60e64cf5676458833f172f01a90ec4b3c0c4d8de
HARSAVARDHINI R <147088869+Harsa-2004@users.noreply.github.com>
2025-06-23 11:31:50 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,3 @@
-// const router=require("express").Router();
-// const { User, validate } = require("../models/User");
-// const bcrypt = require("bcrypt");
-// router.post("/",async(req,res)=>{
-//     try{
-//         const{error}=validate(req.body);
-//         if(error)
-//             return res.status(400).send({message:error.details[0].messaage});
-//         const user=await User.findOne({email:req.body.email});
-//         if (user)
-//             return res.status(409).send({message:"User with given email already exists!"});
-//         const salt=await bcrypt.genSalt(Number(process.env.SALT));
-//         const hashPassword= await bcrypt.hash(req.body.password,salt);
-//         await new User({...req.body, password:hashPassword}).save();
-//         res.status(201).send({message:"User created Successfully"});
-//     }catch(error){
-//         res.status(500).send({message:"Internal Server Error"})
-//     }
-// })
-// module.exports= router;
-
 import express from "express";
 import bcrypt from 'bcryptjs';
 import { User, validate } from "../models/User.js"; // Fix import
